290ebfc3342eeec3584e4e21a6901aeb8f6ddb91
Alexnder77 <Alenilsson7@gmail.com>
2023-01-16 22:21:56 +0800

    Add project files.

---
+++ 5TF048_lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _5TF048_lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
