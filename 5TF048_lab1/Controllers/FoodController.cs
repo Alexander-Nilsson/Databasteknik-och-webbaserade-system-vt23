5a7fb14c95df055c3e06e83d98a0d1574bba4167
Alexnder77 <Alenilsson7@gmail.com>
2023-01-18 16:17:33 +0800

    added finish page

--- 5TF048_lab1/Controllers/FoodController.cs
+++ 5TF048_lab1/Controllers/FoodController.cs
@@ -4,6 +4,7 @@ using _5TF048_lab1.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Newtonsoft.Json;
 
 namespace _5TF048_lab1.Controllers
@@ -25,7 +26,7 @@ namespace _5TF048_lab1.Controllers
             D.Calculate();
 
             string s = JsonConvert.SerializeObject(D);
-            HttpContext.Session.SetString("Food session", s);
+            HttpContext.Session.SetString("Foodsession", s);
 
             return View(D);
         }
@@ -42,6 +43,30 @@ namespace _5TF048_lab1.Controllers
             list.Add(new SelectListItem { Text = "bra", Value = "5" });
 
             return View(list);
+        }
+
+        [HttpPost]
+        public ActionResult Finish(IFormCollection col) {
+            Dinner m = new Dinner();
+            string s = HttpContext.Session.GetString("Foodsession");
+            m = JsonConvert.DeserializeObject<Dinner>(s);
+            int review = Convert.ToInt32(col["Review"]);
+
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            list.Add(new SelectListItem { Text = "uruselt", Value = "0" });
+            list.Add(new SelectListItem { Text = "Dåligt", Value = "1" });
+            list.Add(new SelectListItem { Text = "INTE BRA", Value = "2" });
+            list.Add(new SelectListItem { Text = "ok", Value = "3" });
+            list.Add(new SelectListItem { Text = "inte dåligt", Value = "4" });
+            list.Add(new SelectListItem { Text = "bra", Value = "5" });
+
+            m.Review = list[review].Text;
+
+            s = JsonConvert.SerializeObject(m);
+            HttpContext.Session.SetString("Foodsession", s);
+
+            return View(m);
         }
 
     }
