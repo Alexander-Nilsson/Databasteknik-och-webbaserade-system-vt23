cb4b556b95915c22292636130be473f68e522657
Alexnder77 <Alenilsson7@gmail.com>
2023-01-27 10:13:36 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Laboration_3___Databasdriven_webbapplikation.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
